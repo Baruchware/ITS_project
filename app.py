9e5f42e24e4f7ddf82a6f61ee0be5aab2c26df1e
Baruchware <53627894+Baruchware@users.noreply.github.com>
2019-08-03 00:39:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ from sito.models import Info, User
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db,'User': User}
+    return {'db': db,'User': User}  #per evitare di importare ogni volta nella SHELL FLASK db Post e User, ho scritto queste 3 righe
+                                     #la shell di flask lho usate per creare lo User nella tabella User per poter accedere al sito
